ee648e4e2dcb89de7ecb384e3436e5e2eba1eac2
nohtyp <thomas.foster80@gmail.com>
2014-10-15 16:12:09 -0400

    added required to input as well as pattern matching for acceptable options

--- test.rb
+++ test.rb
@@ -3,55 +3,55 @@ require 'sinatra'
 get '/' do
   %q{<form action="/manage_server" method="post">
                <label for="projectname">Project Name:</label>
-		<input name="post[projectname]" value=''/>
+		<input name="post[projectname]" value=''required />
 		<br>
 		<br>
 		<label for="manage_server">Manage Server:</label>
-		<input list="manage_server" size="10" name="post[manage_server]">
+		<input list="manage_server" size="10" name="post[manage_server]" required pattern='none|vmware|openstack|ovirt'>
 		<datalist id="manage_server">
-			<option value='none'>
-			<option value='vmware'>
-			<option value='openstack'>
-			<option value='ovirt'>
+			<option readonly value='none'>none</option>
+			<option value='vmware'>vmware</option>
+			<option value='openstack'>openstack</option>
+			<option value='ovirt'>ovirt</option>
 		</datalist>
 		<br>
 		<br>
 		<label for="webserver">Webserver:</label>
-		<input list="webserver" size="10" name="post[webserver]">
+		<input list="webserver" size="10" name="post[webserver]" required pattern='none|nginx|apache'>
 		<datalist id='webserver'>
-			<option value='none'>
-			<option value='nginx'>
-			<option value='apache'>
+			<option value='none'>none</option>
+			<option value='nginx'>nginx</option>
+			<option value='apache'>apache</option>
 		</datalist>
 		<br>
 		<br>
 		<label for="application_server">Application Server:</label>
-		<input list="application_server"size="10" name="post[application_server]">
+		<input list="application_server"size="10" name="post[application_server]" required pattern='none|jboss|tomcat|glassfish'>
 	        <datalist id="application_server">
-			<option value='none'>
-			<option value='jboss'>
-		        <option value='tomcat'>
-			<option value='glassfish'>
+			<option value='none'>none</option>
+			<option value='jboss'>jboss</option>
+		        <option value='tomcat'>tomcat</option>
+			<option value='glassfish'>glassfish</option>
 		</datalist>
 		<br>
 		<br>
 		<label for="database_server">Database Server:</label>
-		<input list="database_server" size="10" name="post[database_server]">
+		<input list="database_server" size="10" name="post[database_server]" required pattern='none|mysql|postgresql|sqlserver'>
 		<datalist id="database_server">
-			<option value='none'>
-			<option value='mysql'>
-		        <option value='postgresql'>
-			<option value='sqlserver'>
+			<option value='none'>none</option>
+			<option value='mysql'>mysql</option>
+		        <option value='postgresql'>postgresql</option>
+			<option value='sqlserver'>sqlserver</option>
 		</datalist>
 		<br>
 		<br>
 		<label for="message_queue_server">Message Queue Server:</label>
-		<input list="message_queue_server" size="10" name="post[message_queue_server]">
+		<input list="message_queue_server" size="10" name="post[message_queue_server]" required pattern='none|rabbitmq|activemq|zeromq'>
 	        <datalist id='message_queue_server'>
-			<option value='none'>
-			<option value='rabbitmq'>
-		        <option value='activemq'>
-			<option value='zeromq'>
+			<option value='none'>none</option>
+			<option value='rabbitmq'>rabbitmq</option>
+		        <option value='activemq'>activemq</option>
+			<option value='zeromq'>zeromq</option>
 		</datalist>
 		<br>
 		<br>
@@ -61,11 +61,10 @@ get '/' do
 		<br>
 		<br>
 		<input type='submit' value='build' />
-		<input type='submit' value='cancel' />
-		<input type='submit' value='defaults' />
+		<input type='reset' value='cancel' />
     </form>}
 end
 
 post '/manage_server' do
-  "Options: #{params[:post]}"
+ "Options: #{params[:post]}"
 end
